8cf071419b12b5f93105d3cbed62caf1d441136b
Aakash Jha <42407874+1tsak@users.noreply.github.com>
2022-12-29 15:06:59 +0530

    Added Html & CSS (Search Widget)

---
+++ Hidden Search Widget/script.js
@@ -0,0 +1,3 @@
+const search = document.getElementById("search");
+const searchCont = document.querySelector(".search-container");
+search.addEventListener("click", () => searchCont.classList.add("show"));
